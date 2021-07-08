477891fc1ca05ad9bb35728136099fbb2425c9c1
JINJIN1103 <bonousou@gmail.com>
2021-07-08 04:02:22 +0000

    応用課題5

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
-    @model = params["model"]
     @content = params["content"]
+    @model = params["model"]
     @method = params["method"]
     @records = search_for(@model, @content, @method)
   end
@@ -11,15 +11,23 @@ class SearchController < ApplicationController
     if model == 'user'
       if method == 'perfect'
         User.where(name: content)
-      else
+      elsif method == 'partial'
         User.where('name LIKE ?', '%'+content+'%')
+      elsif method == 'forward'
+        User.where('name LIKE ?', content+'%')
+      elsif method == 'backward'
+        User.where('name LIKE ?', '%'+content)
       end
       
     elsif model == 'book'
       if method == 'perfect'
         Book.where(title: content)
-      else
+      elsif method == 'partial'
         Book.where('title LIKE ?', '%'+content+'%')
+      elsif method == 'forward'
+        Book.where('title LIKE ?', content+'%')
+      elsif method == 'backward'
+        Book.where('title LIKE ?', '%'+content)
       end
     end
   end
